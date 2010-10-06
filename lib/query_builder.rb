108a3011e571e6eb53675dfc3e4cb2afa71be134
Gaspard Bucher <gaspard@teti.ch>
2010-10-06 18:28:43 +0200

    Removed 'using .. parser' message.

--- lib/query_builder.rb
+++ lib/query_builder.rb
@@ -44,10 +44,8 @@ require 'query_builder/query'
 require 'query_builder/error'
 begin
   require 'querybuilder_ext'
-  puts "using C parser"
 rescue LoadError
   require 'querybuilder_rb'
-  puts "using ruby parser"
 end
 require 'query_builder/parser'
 require 'query_builder/processor'
--- lib/query_builder/info.rb
+++ lib/query_builder/info.rb
@@ -1,3 +1,3 @@
 module QueryBuilder
-  VERSION = '0.9.1'
+  VERSION = '0.9.2'
 end
